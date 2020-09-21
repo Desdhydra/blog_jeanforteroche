99fb80c7b13c06d921d5e04a5228664cb76f8a23
Audrey <audrey.joachim@hotmail.com>
2020-09-21 11:29:00 +0200

    Mise à jour des mentions légales

--- view/legal.php
+++ view/legal.php
@@ -7,9 +7,52 @@
     <section>
         <h2>Mentions légales</h2>
         <div class="section-design">
-            <p>Paragraphe 1</p>
-            <p>Paragraphe 2</p>
-            <p>Paragraphe 3</p>
+            <div>
+                <h3>Présentation du site</h3>
+                <p>En vous connectant sur ce site, vous acceptez sans réserve les présentes modalités. Conformément à l’article n°6 de la Loi n°2004-575 du 21 juin 2004 pour la confiance dans l’économie numérique, nous portons à la connaissance des utilisateurs et visiteurs du présent site les informations suivantes :</p>
+                <div>
+                    <h4>Propriétaire du site</h4>
+                    <p>Nom : Jean Forteroche</p>
+                    <p>Adresse : 12, rue de la Plume - 1234 Ireki</p>
+                    <p>E-mail: jeanforteroche@audrey-joachim.com</p>
+                </div>
+                <div>
+                    <h4>Développement</h4>
+                    <p>Nom : Audrey Joachim</p>
+                    <p>E-mail : audrey.joachim.aj@gmail.com</p>
+                </div>
+                <div>
+                    <h4>Hébergement</h4>
+                    <p>Nom : 1&1 IONOS SARL</p>
+                    <p>Adresse : 7, place de la Gare - 57201 Sarreguemines</p>
+                </div>
+            </div>
+            <div>
+                <h3>Description des services fournis</h3>
+                <p>Ce site représente le blog d'un personnage fictif, Jean Forteroche (dont la photo est une image libre de droits). Il a été réalisé par Audrey Joachim dans le cadre d’une formation de développement web dispensée par OpenClassrooms. Il constitue donc un exercice.</p>
+                <p>Merci de respecter son travail et de ne pas poster de commentaires contenant des propos inadaptés ou injurieux.</p>
+            </div>
+            <div>
+                <h3>Conditions d'utilisation</h3>
+                <p>Pour un meilleur confort d’utilisation, nous vous recommandons de recourir à des navigateurs modernes comme Google Chromme, Mozilla Firefox, Microsoft Edge, Safari, Opera, etc.</p>
+                <p>Le propriétaire du site s’efforce de mettre en œuvre tous les moyens dont il dispose pour assurer une information fiable et une mise à jour fiable de son site internet. Toutefois, il ne pourra être tenu responsable si des erreurs ou des omissions pouvaient survenir, qu’elles soient de son fait ou du fait des tiers partenaires qui lui fournissent ces informations. Toutes les informations proposées sur le site sont données à titre indicatif, sont non exhaustives, et sont susceptibles d’évoluer.</p>
+            </div>
+            <div>
+                <h3>Propriété intellectuelle</h3>
+                <p>Tout le contenu du présent sur le site incluant, de façon non limitative, les graphismes, images, textes, logos et icônes ainsi que leur mise en forme sont la propriété exclusive du propriétaire du site, à l'exception des contenus appartenant à d'autres partenaires ou auteurs. Toute reproduction, distribution, modification, adaptation, retransmission ou publication, même partielle, de ces différents éléments est strictement interdite sans l'accord exprès par écrit du propriétaire. Cette représentation ou reproduction, par quelque procédé que ce soit, constitue une contrefaçon sanctionnée par les articles L.335-2 et suivants du Code de la propriété intellectuelle. Le non-respect de cette interdiction constitue une contrefaçon pouvant engager la responsabilité civile et pénale du contrefacteur. En outre, les propriétaires des contenus copiés pourraient intenter une action en justice à votre encontre.</p>
+            </div>
+            <div>
+                <h3>Liens hypertextes et cookies</h3>
+                <p>Le site peut-être amené à vous demander l’acceptation des cookies pour des besoins de statistiques et d'affichage. Un cookie est une information déposée sur votre disque dur par le serveur du site que vous visitez. Il contient plusieurs données qui sont stockées sur votre ordinateur dans un simple fichier texte auquel un serveur accède pour lire et enregistrer des informations. Certaines parties de ce site ne peuvent être fonctionnelles sans l’acceptation de cookies.</p>
+                <p>Le site peut offrir des liens vers d’autres sites internet ou d’autres ressources disponibles sur Internet. Le propriétaire ne dispose d'aucun moyen pour contrôler les sites en connexion avec son propre site. ne répond pas de la disponibilité de tels sites et sources externes, ni ne la garantit. Il ne peut être tenue pour responsable de tout dommage, de quelque nature que ce soit, résultant du contenu de ces sites ou sources externes, et notamment des informations, produits ou services qu’ils proposent, ou de tout usage qui peut être fait de ces éléments. Les risques liés à cette utilisation incombent pleinement à l'internaute, qui doit se conformer à leurs conditions d'utilisation.</p>
+            </div>
+            <div>
+                <h3>Gestion des données personnelles</h3>
+                <p>De manière générale, vous n’êtes pas tenu de nous communiquer vos données personnelles lorsque vous visitez notre site.</p>
+                <p>Le proprietaire du site ne collecte des informations personnelles relatives à l'utilisateur que pour le besoin de certains services proposés par le site. L'utilisateur fournit ces informations en toute connaissance de cause, notamment lorsqu'il procède par lui-même à leur saisie. Conformément aux dispositions des articles 38 et suivants de la loi 78-17 du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés, tout utilisateur dispose d’un droit d’accès, de rectification, de suppression et d’opposition aux données personnelles le concernant. Pour l’exercer, adressez votre demande à jeanforteroche@audrey-joachim.com. Aucune information personnelle de l'utilisateur du site n'est publiée à l'insu de l'utilisateur, échangée, transférée, cédée ou vendue sur un support quelconque à des tiers.</p> 
+                <p>Le présent site est en conformité avec le RGPD.</p>
+                <p>Les bases de données sont protégées par les dispositions de la loi du 1er juillet 1998 transposant la directive 96/9 du 11 mars 1996 relative à la protection juridique des bases de données.</p>
+            </div>
         </div>
     </section>
 
